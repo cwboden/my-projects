e6c49bf3210a009d82a4d292d428b43ef2dc7a33
cwboden <carson.boden@gmail.com>
2017-01-05 11:56:40 -0500

    Updated Python script to stop use of external modules

--- alexaServer/parseSite.py
+++ alexaServer/parseSite.py
@@ -1,35 +1,23 @@
-from urllib2 import urlopen
-from bs4 import BeautifulSoup
 import requests
 import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyQt4.QtWebKit import *
-
-app = QApplication(sys.argv)
 
 loginUrl = "https://login.trueparts.mycccportal.com/"
 visitUrl = "https://trueparts.mycccportal.com/ProcurementSSP/poDashboard.html"
 username = "dboden@cccis.com"
 password = "Steam1er"
 
-def main():
-    session = requests.Session()
+session = requests.Session()
 
-    payload = {
+payload = {
     'USERNAME': username,
     'PASSWORD': password,
     'submit': 'id',
-    }
+}
 
-    print payload
+print payload
 
-    r = session.post(loginUrl, data = payload)
-    r = session.get(visitUrl)
+r = session.post(loginUrl, data = payload)
+print r.text
 
-    web = QWebView()
-    web.load(QUrl(visitUrl))
-    web.show()
-
-if __name__ == '__main__':
-    main()
+r = session.get(visitUrl)
+print r.text
